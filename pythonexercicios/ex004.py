134691d18801b2441fc162d2d17ff2621c6487de
danisayumin <sayuminitta@gmail.com>
2022-03-30 14:29:00 -0300

    Create ex004.py
    tipos primitivos

---
+++ pythonexercicios/ex004.py
@@ -0,0 +1,8 @@
+#tipos primitivos
+a = input('digite algo:')
+print('o tipo primitivo desse valor é', type(a))
+print('só tem espaço?', a.isspace())
+print('é alfabético?', a.isalpha())
+print('é um numero?', a.isnumeric())
+print('é alfanumerico?',a.isalnum())
+print('é capitalizada?',a.istitle())
