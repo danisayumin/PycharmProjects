b44f502d1dead3c5edad670dde2b30b1e39577d1
danisayumin <sayuminitta@gmail.com>
2022-03-30 14:20:19 -0300

    Create Aula007A.py
    soma de valores int

---
+++ Aula007/Aula007A.py
@@ -0,0 +1,9 @@
+n1 = int (input('um valor:'))
+n2 = int(input('outro valor:'))
+s= n1+n2
+m= n1 * n2
+d= n1 / n2
+di= n1 // n2
+e= n1 ** n2
+print('a soma vale {}, o produto é {},a divisão é {}'.format( s, m, d))
+print('a divisão interira vale {} e a potencia {}'.format(di, e))
