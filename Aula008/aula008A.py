e7a8a4e99dc6095ba723f1d65f4ffc95dbde2662
danisayumin <sayuminitta@gmail.com>
2022-03-30 14:21:19 -0300

    Create aula008A.py
    lov emoji

---
+++ Aula008/aula008A.py
@@ -0,0 +1,5 @@
+import emoji
+print(emoji.emojize('ti amu Heitor:red_heart:',use_aliases=True))
+print(emoji.emojize("ti amu muito mozin :red_heart:",variant="emoji_type"))
+print(emoji.emojize('mozinho is :thumbs_up:', use_aliases=True))
+
