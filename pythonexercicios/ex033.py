6b5ae8ce33f4edd7f38a7bb888f757cb404e1aae
danisayumin <sayuminitta@gmail.com>
2022-03-30 14:45:38 -0300

    Create ex033.py
    lista de nomes

---
+++ pythonexercicios/ex033.py
@@ -0,0 +1,5 @@
+n1: int = int(input('digite o primeiro numero:'))
+n2 = int(input(" digite o segundo numero:"))
+n3 = int(input('digite o terceiro numero:'))
+lista = [n1, n2, n3]
+print(f'o maior numero sera : {max(lista)} E o menor sera : {min(lista)}')
