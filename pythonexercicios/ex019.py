f5377dfae62a9e84eb67852b730bf0c0c2390e8c
danisayumin <sayuminitta@gmail.com>
2022-03-30 14:39:36 -0300

    Create ex019.py
    import random
    sorteio

---
+++ pythonexercicios/ex019.py
@@ -0,0 +1,7 @@
+import random
+n1 = str(input('primeiro aluno:'))
+n2 = str(input('segundo aluno:'))
+n3 = str(input('terceiro aluno:'))
+lista = (n1,n2,n3)
+escolhido = random.choice(lista)
+print('o aluno escolhido foi {}'.format(escolhido))
